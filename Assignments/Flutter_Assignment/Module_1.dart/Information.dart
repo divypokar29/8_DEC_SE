188e5f5e0b36e1eff298c49ada4a4d66407c2254
divypokar29 <divypokar29@gmail.com>
2026-03-23 14:12:29 +0530

    delete some

--- Assignments/Flutter_Assignment/Module_1.dart/Information.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-  print("Name: Divy Patel");
-  print("Birth Date: 01-01-2000");
-  print("Age: 25");
-  print("Address: Ahmedabad");
-}
--- Assignments/Flutter_Assignment/Module_1.dart/Square_and_Cube.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() {
-  print("Enter number:");
-  int n = int.parse(stdin.readLineSync()!);
-
-  print("Square = ${n * n}");
-  print("Cube = ${n * n * n}");
-}
